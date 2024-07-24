f30d585e4fafb9110ad000a741b25e35d19bc5cd
AdielSM <adielsobral76@gmail.com>
2024-07-23 22:30:45 -0300

    Header + Start Css Nativo

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,29 @@
+import logo from '../../../public/Logo_Lara.png';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../style/header.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
+export default function Header() {
+    return (
+    <header className={styles.header}>
+        <div>
+            <Image src={logo} alt="Logo Lara" />
+        </div>
+        <div className={styles.nav}>
+            <nav className={styles.navBar}>
+                <a href="">Home</a>
+                <a href="">Sobre Nós</a>
+                <a href="">Projetos</a>
+                <a href="">Equipe</a>
+            </nav>
+            <button className={styles.contact_button}>
+                Contato
+                <FontAwesomeIcon className={styles.contact_button_arrow} 
+                icon={faArrowRight}/>
+                </button>
+        </div>
+    </header>
+    );
+    }
