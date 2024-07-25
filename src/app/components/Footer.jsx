89b7a554793bf6dcab4c0b763bc816bc6bb15cb1
alessandro rodrigues <alessandjr0@gmail.com>
2024-07-25 19:39:20 -0400

    footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import logo from "../../../public/Logo_Lara.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons'
+import styles from '../style/footer.module.css'
+
+
+export default function Footer(){
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.footer_container}>
+                 <div className={styles.column}>  
+                    <div className={styles.footer_logo}>
+                        <Image src={logo} alt="logo"/>
+                    </div>
+                    <div className={styles.footer_info}>
+                        <p>Copyright 2024</p>
+                        <p>Todos os direitos reservados</p>
+                    </div>
+                    <div className={styles.footer_social}>
+                        <FontAwesomeIcon icon={faInstagram} className={styles.footer_social_icons} />
+                        <FontAwesomeIcon icon={faXTwitter} className={styles.footer_social_icons} />
+                        <FontAwesomeIcon icon={faYoutube} className={styles.footer_social_icons} />
+                    </div>
+                  </div>
+            
+                <div className={styles.column}>
+                    <div className={styles.footer_links}>
+                        <h1>Lara</h1>
+                        <nav>
+                            <a href="#">Home</a>
+                            <a href="#">Sobre nós</a>
+                            <a href="#">Projetos</a>
+                            <a href="#">Equipe</a>
+                            <a href="#">Entre em contato</a>
+                        </nav>
+                    </div>
+            </div>
+
+
+                <div className={styles.column}>
+                    <div className={styles.footer_subscribe}>
+                        <h1>Mantenha-se atualizado</h1>
+                        <div>
+                            <input type="email" name="email" id="email" placeholder="Seu Endereço de Email" className={styles.input} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </footer> 
+    );
+}
