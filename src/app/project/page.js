74adac6e647370a4f9f9919e574fe05c8536e42e
Bruno <brunovn.eneas@gmail.com>
2024-07-30 15:40:23 -0300

    HTML da pagina 'projeto' feito.

---
+++ src/app/project/page.js
@@ -0,0 +1,82 @@
+export default function page(){
+    return(
+        <main>
+            <section>
+                <div>
+                    <h1>Nome do Projeto</h1>
+                    <p>Instituto Federal da Paraiba - Projeto n°0000</p>
+                    <hr/>
+                </div>
+            </section>
+            <section>
+                <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </div>
+                {/* Imagem aqui */}
+            </section>
+            <section>
+                <h2>Metodologia Utilizadas</h2>
+                <div>
+                    {/* Imagem */}
+                    <p>Lorem Ipsum Dolor</p>
+                    {/* Imagem */}
+                    <p>Lorem Ipsum Dolor</p>
+                    {/* Imagem */}
+                    <p>Lorem Ipsum Dolor</p>
+                </div>
+            </section>
+            <section>
+                <h2>Reconhecimentos e Divulgação</h2>
+                <div>
+                    <div>
+                        {/* Imagem */}
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div>
+                        {/* Imagem */}
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div>
+                        {/* Imagem */}
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div>
+                        {/* Imagem */}
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <h2>Equipe de Pesquisadores</h2>
+                <div>
+                    <div>
+                        <h3>Bolsistas</h3>
+                        <div>
+                            <p>Bruno Vinicius de Araujo Eneas</p>
+                            <hr/>
+                            <p>Bruno Vinicius de Araujo Eneas</p>
+                            <hr/>
+                            <p>Bruno Vinicius de Araujo Eneas</p>
+                            <hr/>
+                            <p>Bruno Vinicius de Araujo Eneas</p>
+                            <hr/>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>Voluntários</h3>
+                        <div>
+                        <p>Bruno Vinicius de Araujo Eneas</p>
+                            <hr/>
+                            <p>Bruno Vinicius de Araujo Eneas</p>
+                            <hr/>
+                            <p>Bruno Vinicius de Araujo Eneas</p>
+                            <hr/>
+                            <p>Bruno Vinicius de Araujo Eneas</p>
+                            <hr/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
